704580d43f12c4d6cb6be915463f87277c6d1b2c
CMGS@CMGS-Null <CMGS@CMGS-Null>
2012-01-06 21:50:14 +0800

    test old code

--- app.py
+++ app.py
@@ -1,37 +1,61 @@
 #!/usr/bin/python
 # encoding: UTF-8
 
-from lib.weibo import APIClient
-from config import APP_KEY, APP_SECRET, CALLBACK_URL
-from flask import Flask, render_template, redirect, request, session
+from config import APP_KEY, APP_SECRET
 from sheep.api.statics import static_files
+
+from flaskext.oauth import OAuth
+from flask import Flask, render_template, redirect, request, session, url_for
 
 app = Flask(__name__)
 app.debug = True
 app.jinja_env.filters['s_files'] = static_files
 app.secret_key = 'sheep!@$user!#$%^'
 
+oauth = OAuth()
+
+weibo = oauth.remote_app('weibo',
+    base_url='http://api.t.sina.com.cn',
+    request_token_url='http://api.t.sina.com.cn/oauth/request_token',
+    access_token_url='http://api.t.sina.com.cn/oauth/access_token',
+    authorize_url='http://api.t.sina.com.cn/oauth/authorize',
+    consumer_key=APP_KEY,
+    consumer_secret=APP_SECRET
+)
+
 @app.route('/')
 def index():
-    if session.get('isLogin') is None:
+    if session.get('isLogin', None):
         return render_template('index.html')
     else:
-        code = request.args.get('code')
-        client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-        r = client.request_access_token(code)
-        access_token = r.access_token
-        expires_in = r.expires_in
-        client.set_access_token(access_token, expires_in)
-        return client.get.statuses__user_timeline()
+        return 'success'
 
 @app.route('/Login')
 def login():
-    session['isLogin'] = 1
-    return redirect('/')
+    session['isLogin'] = True
+    return 'logined'
 
-@app.route('/weibo')
+@app.route('/Login/Weibo')
 def weibo_login():
-    client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-    url = client.get_authorize_url()
-    return redirect(url)
+    return weibo.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
 
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        print u'You denied the request to sign in.'
+        return redirect(next_url)
+    session['weibo_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['isLogin'] = True
+    print 'You were signed in'
+    return redirect(next_url)
+
+@twitter.tokengetter
+def get_weibo_token():
+    if not session.get('isLogin', None):
+        return session['oauth_token'], session['user.oauth_secret']
--- config.py
+++ config.py
@@ -3,7 +3,6 @@
 
 APP_KEY = '619662253'
 APP_SECRET = 'b1d02f2ff16aec904d835d34bc926ae7'
-CALLBACK_URL = 'http://xiaomen.co/Login'
 
 try:
     from local_config import *
