7ba50d7b44dbf74a90f687dd437f7062a1e7189e
CMGS <ilskdw@gmail.com>
2012-01-06 18:12:47 +0800

    test sina

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 # encoding: UTF-8
 
+from lib.weibo import APIClient
+from config import APP_KEY, APP_SECRET, CALLBACK_URL
+from flask import Flask, render_template, redirect, request, session
 from sheep.api.statics import static_files
-from flask import Flask, request, redirect, session, render_template
-from lib.weibo import OAuthHandler, oauth, API
 
 app = Flask(__name__)
 app.debug = True
-app.secret_key = 'sheep!user#$@!v1'
 app.jinja_env.filters['s_files'] = static_files
 
-consumer_key = '619662253'
-consumer_secret = 'b1d02f2ff16aec904d835d34bc926ae7'
-
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    if session.get('isLogin') is None:
+        return render_template('index.html')
+    else:
+        code = request.get('code')
+        client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
+        r = client.request_access_token(code)
+        access_token = r.access_token
+        expires_in = r.expires_in
+        client.set_access_token(access_token, expires_in)
+        return client.get.statuses__user_timeline()
+
+@app.route('/Login')
+def login():
+    session['isLogin'] = 1
+    return redirect('/')
+
+@app.route('/weibo')
+def weibo_login():
+    client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
+    url = client.get_authorize_url()
+    return redirect(url)
 
---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python2.7
+#coding:utf-8
+
+APP_KEY = '619662253'
+APP_SECRET = 'b1d02f2ff16aec904d835d34bc926ae7'
+CALLBACK_URL = 'http://xiaomen.co/Login'
+
+try:
+    from local_config import *
+except:
+    pass
