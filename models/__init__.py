af2cd107284380f93802bf62c35f743e36bf98c2
CMGS <ilskdw@gmail.com>
2012-01-16 10:58:33 +0800

    recyle mysql connection

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # setup sqlalchemy
-engine = create_engine(config.DATABASE_URI)
+engine = create_engine(config.DATABASE_URI, pool_recycle=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
