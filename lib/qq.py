a8cbc247dc31e9cdc635f39254e6596821d0055b
CMGS <ilskdw@gmail.com>
2012-01-18 17:11:48 +0800

    test

--- lib/qq.py
+++ lib/qq.py
@@ -39,6 +39,7 @@ class QQOAuth(OAuthRemoteApp):
         url = add_query(self.expand_url(self.access_token_url), remote_args)
         resp, content = self._client.request(url, self.access_token_method)
         data = json.loads(content[content.find('(')+1 : content.find(')')].strip())
+        print data
         if data.get('error', None):
             logger.info(data)
             raise OAuthException('Invalid response from ' + self.name, data)
