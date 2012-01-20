e55bde6f74e710deb10b9ae67f69f7c6fd404946
CMGS <ilskdw@gmail.com>
2012-01-20 13:58:09 +0800

    clean douban views

--- views/douban.py
+++ views/douban.py
@@ -10,5 +10,7 @@ douban_oauth_login = Base_OAuth_Login('douban', douban, 'douban_user_id')
 
 douban.tokengetter(douban_oauth_login.get_token)
 douban_oauth.add_url_rule('/Login', view_func=douban_oauth_login.login)
-douban_oauth.add_url_rule('/Authorized', view_func=douban.authorized_handler(douban_oauth_login.authorized))
+douban_oauth.add_url_rule('/Authorized', view_func=\
+        douban.authorized_handler(douban_oauth_login.authorized)
+)
 
