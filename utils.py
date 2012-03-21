f79104064ea71b312dd1a556800b40a097847f5b
CMGS <ilskdw@mspil.edu.cn>
2012-03-21 13:55:20 +0800

    clean code.

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from flask import g
 from models import db, User
 
 def get_current_user():
-    if not g.user:
+    if not g.session or not g.session.get('user_id') or not g.session.get('user_token'):
         return None
     user = User.query.get(g.session['user_id'])
     if not user or g.session['user_token'] != user.token:
