626e19c660c3a54cc7ce8120c95105c901e8b82d
CMGS <ilskdw@mspil.edu.cn>
2012-08-12 19:34:01 +0800

    auto redirect when login required

--- utils/account.py
+++ utils/account.py
@@ -2,7 +2,7 @@
 #coding:utf-8
 
 from functools import wraps
-from flask import g, url_for, redirect
+from flask import g, url_for, redirect, request
 
 def login_required(next=None, need=True, *args, **kwargs):
     def _login_required(f):
@@ -11,7 +11,11 @@ def login_required(next=None, need=True, *args, **kwargs):
             if (need and not g.current_user) or \
                     (not need and g.current_user):
                 if next:
-                    return redirect(url_for(next, *args, **kwargs))
+                    if next != 'account.login':
+                        url = url_for(next)
+                    else:
+                        url = url_for(next, redirect=request.url)
+                    return redirect(url)
                 return redirect(url_for('index'))
             return f(*args, **kwargs)
         return _
