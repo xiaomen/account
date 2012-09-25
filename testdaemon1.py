18b395062dc3b9bc55382129af0b3f29f09258ad
CMGS <ilskdw@gmail.com>
2012-09-25 15:19:27 +0800

    add more daemon test

--- testdaemon1.py
+++ testdaemon1.py
@@ -3,11 +3,17 @@
 
 import logging
 import datetime, time
+from sheep.api.mysql import connect, get_mysql_conn_params
 
 logger = logging.getLogger()
 
 def main():
     while 1:
+        d = connect()
+        c = d.cursor()
+        c.execute('SELECT * FROM users;')
+        print c.fetchall()
+        print get_mysql_conn_params()
         logger.info(datetime.datetime.now())
         time.sleep(5)
 
