87e62542cc3d6d762999fdd522d4936d2c5ef854
CMGS <ilskdw@gmail.com>
2012-11-02 11:46:13 +0800

    dump mysql schema

---
+++ databases/schema.sql
@@ -0,0 +1,57 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forget` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `stub` char(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `stub` (`stub`),
+  KEY `ix_forget_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_uid` int(11) DEFAULT NULL,
+  `to_uid` int(11) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `content` text,
+  `is_read` int(11) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `inbox` int(11) DEFAULT NULL,
+  `outbox` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_mail_is_read` (`is_read`),
+  KEY `ix_mail_from_uid` (`from_uid`),
+  KEY `ix_mail_to_uid` (`to_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `oauth_type` varchar(20) DEFAULT NULL,
+  `oauth_uid` varchar(200) DEFAULT NULL,
+  `oauth_token` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_oauth_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(16) NOT NULL,
+  `passwd` char(50) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `token` char(16) DEFAULT NULL,
+  `domain` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `domain` (`domain`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
