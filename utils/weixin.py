64ce1583235debc61a9f977383ada6c536092319
CMGS <ilskdw@gmail.com>
2013-04-11 17:35:45 +0800

    add robot

--- utils/weixin.py
+++ utils/weixin.py
@@ -8,9 +8,7 @@ from pyquery import PyQuery as pq
 from flask import abort
 
 from config import WEIXIN_TOKEN
-
 from utils.token import get_uid
-
 from query.account import get_user, clear_user_cache
 
 class Message(object):
@@ -42,14 +40,45 @@ def check_keys(src, keys):
             logging.warning("missing %s", key)
             raise abort(400)
 
-def check_code(code, message):
-    user = get_uid(code)
-    if not user:
-        return "绑定失败，请检查验证码或者返回绑定页面刷新获取新的验证码。"
-    u = get_user(user) 
-    if not u:
-        return "绑定失败，请检查验证码或者返回绑定页面刷新获取新的验证码。"
-    u.set_weixin(message.From)
-    clear_user_cache(u)
-    return "绑定成功！"
+class robot(object):
+    def __init__(self):
+        self._commands = []
+        commands = dir(self)
+        for command in commands:
+            if command.startswith('_'):
+                continue
+            self._commands.append(command)
+
+    def bind(self, body, message):
+        '''绑定校门口账号, 格式为「-code TOKEN」或者是「bind TOKEN」'''
+        user = get_uid(body)
+        if not user:
+            return "绑定失败，请检查验证码或者返回绑定页面刷新获取新的验证码。"
+        u = get_user(user)
+        if not u:
+            return "绑定失败，请检查验证码或者返回绑定页面刷新获取新的验证码。"
+        u.set_weixin(message.From)
+        clear_user_cache(u)
+        return "绑定成功！"
+
+    def unbind(self, body, message):
+        '''undefine'''
+        pass
+
+    def who(self, body, message):
+        '''undefine'''
+        pass
+
+    def repeat(self, body, message):
+        '''Repeat what u say'''
+        return body
+
+    def help(self, command, message):
+        '''显示可用命令，例如「help」，或者是「help bind」'''
+        if not command.strip():
+            return ' '.join(self._commands)
+        if command not in self._commands:
+            return '没有这个命令哦'
+        else:
+            return getattr(self, command).__doc__
 
--- views/weixin.py
+++ views/weixin.py
@@ -7,12 +7,16 @@ from flask import abort, request, Blueprint
 from flaskext.csrf import csrf_exempt
 
 from utils.weixin import Message, compute_signature, \
-        return_message, check_code, check_keys
+        return_message, check_keys, Robot
 
 logger = logging.getLogger(__name__)
 weixin = Blueprint('weixin', __name__)
 
 class WeiXin(MethodView):
+    def __init__(self):
+        self.robot = Robot()
+        super(WeiXin, self).__init__()
+
     def get(self):
         args = request.args.items()
         check_keys(args, ["signature", "timestamp", "nonce", "echostr"])
@@ -24,13 +28,17 @@ class WeiXin(MethodView):
 
     def post(self):
         msg = Message(request.data)
-        msg_splited = msg.Body.split(" ")
-        if msg_splited[0] == "-code":
-            if len(msg_splited) == 2:
-                return return_message(msg.To, msg.From, check_code(msg_splited[1], msg))
-            else:
-                return return_message(msg.To, msg.From, "绑定失败，请检查验证码格式。")
-        return ""
+        msg_splited = msg.Body.split(' ', 1)
+        if len(msg_splited) < 2:
+            msg_splited.append(' ')
+        command, body = msg_splited
+        command = command.lower()
+        if command == "-code":
+            command == "bind"
+        robot = getattr(self.robot, command, None)
+        if not robot:
+            return return_message(msg.To, msg.From, '命令无法识别')
+        return return_message(msg.To, msg.From, robot(body, msg))
 
 weixin_view = WeiXin.as_view('weixin')
 weixin.add_url_rule('/', view_func=weixin_view, methods=['GET'])
